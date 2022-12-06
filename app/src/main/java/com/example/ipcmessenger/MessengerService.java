250853b7c202b93d1984f67927bcc22c9fcd8c7d
yn030727 <2601098388@qq.com>
2022-12-06 23:28:06 +0800

    1.test

--- app/src/main/java/com/example/ipcmessenger/MessengerService.java
+++ app/src/main/java/com/example/ipcmessenger/MessengerService.java
@@ -1,10 +1,46 @@
 package com.example.ipcmessenger;
 
+import static android.icu.text.UnicodeSet.CASE;
+
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+
+import androidx.annotation.NonNull;
 
 public class MessengerService extends Service {
+
+    private static class MessengerHandler extends Handler{
+        //处理服务端发送过来的信息
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what){
+                case 1:{
+                    Messenger client = msg.replyTo;
+                    Message replyMessage = Message.obtain(null,1);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("reply","嗯，你的消息我收到了");
+                    replyMessage.setData(bundle);
+                    try {
+                        client.send(replyMessage);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+                default:{
+                    super.handleMessage(msg);
+                }
+            }
+        }
+    }
+
+
     public MessengerService() {
     }
 
